9d3ccd6eb57de4e9b9de8d2d850d02450d350f2b
ajaydev2002 <ajay.p@rigelsoft.in>
2025-03-13 16:26:54 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/Header.jsx";
 import Dashboard from './components/Dashboard.jsx';
-import Authentication from './components/Authentication';
+import Authentication from './components/Authentication.jsx';
 import Error from "./components/Error.jsx";
 
 
