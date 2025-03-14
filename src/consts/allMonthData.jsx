10d1a31f452ab135c95b09bbbbbe0d963358da46
ajaydev2002 <ajay.p@rigelsoft.in>
2025-03-14 11:36:42 +0530

    removed files

--- src/consts/allMonthData.jsx
+++ src/consts/allMonthData.jsx
@@ -1,17 +1,77 @@
 const allMonthData = [
 
-    { Month: "Jan", Orders: 34, Earnings: 89.25, Refunds: 8 },
-    { Month: "Feb", Orders: 65, Earnings: 98.58, Refunds: 12 },
-    { Month: "Mar", Orders: 46, Earnings: 68.74, Refunds: 7 },
-    { Month: "Apr", Orders: 68, Earnings: 108.87, Refunds: 17 },
-    { Month: "May", Orders: 49, Earnings: 77.54, Refunds: 21 },
-    { Month: "Jun", Orders: 61, Earnings: 84.03, Refunds: 11 },
-    { Month: "Jul", Orders: 42, Earnings: 51.24, Refunds: 5 },
-    { Month: "Aug", Orders: 44, Earnings: 28.57, Refunds: 9 },
-    { Month: "Sep", Orders: 78, Earnings: 92.57, Refunds: 7 },
-    { Month: "Oct", Orders: 52, Earnings: 42.36, Refunds: 29 },
-    { Month: "Nov", Orders: 63, Earnings: 88.51, Refunds: 12 },
-    { Month: "Dec", Orders: 67, Earnings: 36.57, Refunds: 35 },
+    {
+        Month: "Jan",
+        Orders: 34,
+        Earnings: 89.25,
+        Refunds: 8
+    },
+    {
+        Month: "Feb",
+        Orders: 65,
+        Earnings: 98.58,
+        Refunds: 12
+    },
+    {
+        Month: "Mar",
+        Orders: 46,
+        Earnings: 68.74,
+        Refunds: 7
+    },
+    {
+        Month: "Apr",
+        Orders: 68,
+        Earnings: 108.87,
+        Refunds: 17
+    },
+    {
+        Month: "May",
+        Orders: 49,
+        Earnings: 77.54,
+        Refunds: 21
+    },
+    {
+        Month: "Jun",
+        Orders: 61,
+        Earnings: 84.03,
+        Refunds: 11
+    },
+    {
+        Month: "Jul",
+        Orders: 42,
+        Earnings: 51.24,
+        Refunds: 5
+    },
+    {
+        Month: "Aug",
+        Orders: 44,
+        Earnings: 28.57,
+        Refunds: 9
+    },
+    {
+        Month: "Sep",
+        Orders: 78,
+        Earnings: 92.57,
+        Refunds: 7
+    },
+    {
+        Month: "Oct",
+        Orders: 52,
+        Earnings: 42.36,
+        Refunds: 29
+    },
+    {
+        Month: "Nov",
+        Orders: 63,
+        Earnings: 88.51,
+        Refunds: 12
+    },
+    {
+        Month: "Dec",
+        Orders: 67,
+        Earnings: 36.57,
+        Refunds: 35
+    },
 ];
 
 export default allMonthData;
